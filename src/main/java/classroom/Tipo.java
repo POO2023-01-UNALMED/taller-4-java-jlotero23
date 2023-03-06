c43896bf98a19078c8e2ce156287a13996cfa829
jlotero23 <125835077+jlotero23@users.noreply.github.com>
2023-03-06 15:19:28 -0500

    Cambios
    Esto

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -3,9 +3,13 @@ package classroom;
 public enum Tipo {
 	DISCIPLINAR(10),FUNDAMENTACION(20),ELECTIVA(30);
 	
+	Tipo(int i) {
+		
+	}
+	
 	private int codigo;
 	private String nombre;
-	public Tipo(int codigo,String nombre) {
+    private Tipo(int codigo,String nombre) {
 		this.codigo = codigo;
 		this.nombre = nombre;
 	}
